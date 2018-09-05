3a26968973e3232750fde987729b502c97e4b65d
wangxiaoliang <15618359568@163.com>
2018-09-05 16:57:16 +0800

    将service-provider注册到eureka上

--- LeYouSpringClouldTest/brighton-service-provider/src/main/java/cn/brighton/service/BrightonServiceProviderApplication.java
+++ LeYouSpringClouldTest/brighton-service-provider/src/main/java/cn/brighton/service/BrightonServiceProviderApplication.java
@@ -2,8 +2,10 @@ package cn.brighton.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 @MapperScan("cn.brighton.service.mapper")
 @SpringBootApplication
 public class BrightonServiceProviderApplication {
