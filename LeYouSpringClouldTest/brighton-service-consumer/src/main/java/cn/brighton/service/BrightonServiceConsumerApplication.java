d2bcbe8ee26d0fcbcc5d40f1e45c1c8185de1a4b
wangxiaoliang <15618359568@163.com>
2018-09-05 17:20:09 +0800

    将service-consumer注册到eureka上,并获取服务

--- LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/BrightonServiceConsumerApplication.java
+++ LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/BrightonServiceConsumerApplication.java
@@ -2,10 +2,12 @@ package cn.brighton.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient //开启Eureka客户端
 @SpringBootApplication
 public class BrightonServiceConsumerApplication {
 
--- LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/dao/UserDao.java
+++ LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/dao/UserDao.java
@@ -3,8 +3,12 @@ package cn.brighton.service.dao;
 
 import cn.brighton.service.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * <p>
@@ -18,8 +22,18 @@ public class UserDao {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient; //eureka客户端,可以获取到eureka中的服务信息
+
     public User queryUserById(Long id){
-        String url = "http://localhost:8081/user/"+id;
-        return this.restTemplate.getForObject(url,User.class);
+//        String url = "http://localhost:8081/user/"+id;
+//        return this.restTemplate.getForObject(url,User.class);
+        //根据服务名称,获取服务实例.有可能是集群,所以是service实例集合
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        //因为只有一个UserService.所以获取第一个实例
+        ServiceInstance instance = instances.get(0);
+        //获取ip和端口信息,拼接成服务地址
+        String baseURL= "http://"+instance.getHost()+":"+instance.getPort()+"/user/"+id;
+        return restTemplate.getForObject(baseURL,User.class);
     }
 }
