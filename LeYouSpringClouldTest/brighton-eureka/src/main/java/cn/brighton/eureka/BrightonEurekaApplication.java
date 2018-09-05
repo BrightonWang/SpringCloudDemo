ba256b401a82d95407d6c31121ee0ac4ef02054b
wangxiaoliang <15618359568@163.com>
2018-09-05 16:10:13 +0800

    提交eureka-server

---
+++ LeYouSpringClouldTest/brighton-eureka/src/main/java/cn/brighton/eureka/BrightonEurekaApplication.java
@@ -0,0 +1,14 @@
+package cn.brighton.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//声明这个应用是一个EurekaServer
+public class BrightonEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrightonEurekaApplication.class, args);
+    }
+}
