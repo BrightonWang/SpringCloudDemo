e740a95a47a3aeee2ce4810fc8e273a8495a1870
wangxiaoliang <15618359568@163.com>
2018-09-13 10:36:37 +0800

    实现熔断器异常处理并使重试机制恢复

--- LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/service/UserService.java
+++ LeYouSpringClouldTest/brighton-service-consumer/src/main/java/cn/brighton/service/service/UserService.java
@@ -20,8 +20,8 @@ import java.util.List;
  */
 @Service
 public class UserService {
-//    @Autowired
-//    private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
     @Autowired
     private RestTemplate restTemplate;
 
@@ -31,21 +31,21 @@ public class UserService {
     public List<User> queryUserByIds(List<Long> ids){
         List<User> users = new ArrayList<>();
         //地址直接写服务名称即可
-        String baseUrl = "http://user-service/user/";
-        ids.forEach(id->{
+//        String baseUrl = "http://user-service/user/";
+//        ids.forEach(id->{
             //我们测试多次查询
-            users.add(this.restTemplate.getForObject(baseUrl+id, User.class));
+//            users.add(this.restTemplate.getForObject(baseUrl+id, User.class));
             //每次间隔500毫秒
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-//        for (Long id : ids){
-//            User user = userDao.queryUserById(id);
-//            users.add(user);
-//        }
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+        for (Long id : ids){
+            User user = userDao.queryUserById(id);
+            users.add(user);
+        }
         return users;
     }
 }
