c3c2069492adce109a18704e1f8938c17b66a46f
wangxiaoliang <15618359568@163.com>
2019-04-12 10:39:14 +0800

    eureka提交

---
+++ NewSpringCloudDemo/brighton-eureka/src/main/java/com/brighton/eureka/BrightonEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.brighton.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightonEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrightonEurekaApplication.class, args);
+    }
+
+}
