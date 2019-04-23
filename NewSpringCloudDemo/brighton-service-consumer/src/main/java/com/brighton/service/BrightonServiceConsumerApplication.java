b89b845d77c40a7326231f1ac3720242b7bf2a8a
wangxiaoliang <15618359568@163.com>
2019-04-23 16:32:25 +0800

    使用feign

--- NewSpringCloudDemo/brighton-service-consumer/src/main/java/com/brighton/service/BrightonServiceConsumerApplication.java
+++ NewSpringCloudDemo/brighton-service-consumer/src/main/java/com/brighton/service/BrightonServiceConsumerApplication.java
@@ -5,20 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
+@EnableHystrix //开启熔断
+@EnableFeignClients //开启fegin客户端
 public class BrightonServiceConsumerApplication {
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        //这次我们使用了okHttp客户端,只需注入工厂即可
-        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
-    }
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate(){
+//        //这次我们使用了okHttp客户端,只需注入工厂即可
+//        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(BrightonServiceConsumerApplication.class, args);
