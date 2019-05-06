79c736a12066d33079982f189a8314f15c3cebb7
wangxiaoliang <15618359568@163.com>
2019-05-06 10:18:30 +0800

    添加user-consumer网关

--- NewSpringCloudDemo/brighton-service-zuul/src/main/java/com/brighton/zuul/BrightonZuulApplication.java
+++ NewSpringCloudDemo/brighton-service-zuul/src/main/java/com/brighton/zuul/BrightonZuulApplication.java
@@ -2,12 +2,14 @@ package com.brighton.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy // 开启网关功能
 @EnableDiscoveryClient
+@SpringCloudApplication
 public class BrightonZuulApplication {
 
     public static void main(String[] args) {
