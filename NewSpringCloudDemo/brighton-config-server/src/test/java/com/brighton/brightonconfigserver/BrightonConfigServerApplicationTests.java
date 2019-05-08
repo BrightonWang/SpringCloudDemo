878b6d034ff0d74b542bb56028d015c1d7640a18
wangxiaoliang <15618359568@163.com>
2019-05-08 21:37:23 +0800

    springCloudConfigServer

---
+++ NewSpringCloudDemo/brighton-config-server/src/test/java/com/brighton/brightonconfigserver/BrightonConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brighton.brightonconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrightonConfigServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
